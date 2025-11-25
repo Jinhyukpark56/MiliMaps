887c9a24e5f27cda74ae65033963aba81f1c91cb
kskbsu <kskbsu@bu.ac.kr>
2025-11-25 14:07:25 +0900

    탭바 메뉴 아이콘 변경(부트스트랩 아이콘 패키지 사용), 탭바 레이아웃(너비) 조정

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from "./App";
 
 import "./styles/variables.css"; // 색상 변수
 import "./styles/global.css"; // 공통 레이아웃 스타일
+import 'bootstrap-icons/font/bootstrap-icons.css'; // 부트스트랩 아이콘 스타일
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
